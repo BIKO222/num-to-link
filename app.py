c8bad0fedf64cf1f274a84e1bf6ab4c5d017cfe7
Beibarys <152636697+BIKO222@users.noreply.github.com>
2025-04-20 13:23:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,25 @@
 import os
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import ParseMode
+from aiogram.enums import ParseMode
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.utils import executor
 from dotenv import load_dotenv
 import urllib.parse
 
-# Загружаем токен из .env
+
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 
-# Создаем экземпляры бота и диспетчера
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
 
-# Стартовая команда
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     await message.reply("Привет! Отправь мне номер телефона (и по желанию сообщение), и я сделаю ссылку WhatsApp.")
-
-# Обработка текстовых сообщений
 @dp.message_handler(lambda message: message.text)
 async def handle_message(message: types.Message):
     text = message.text.strip()
-
     parts = text.split(maxsplit=1)
     if not parts:
         await message.reply("Отправь номер телефона.")
